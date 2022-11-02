f2d6240b727904110397e3c6ba8b28cd5e205344
Billy.Zheng <vil963@gmail.com>
2022-11-02 22:15:04 +0800

    Move binary to ./bin folder

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 set -e
 
 ruby test/test.rb && echo "Running ruby test.rb successful."
-chmod +x ./port_ruby_to_crystal
-./port_ruby_to_crystal --test
+chmod +x ./bin/port_ruby_to_crystal
+./bin/port_ruby_to_crystal --test
 crystal run test/test.cr && echo "Running crystal run test.cr successful."
